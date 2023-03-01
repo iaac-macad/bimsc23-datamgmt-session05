86e8897ba18ea8ea69532b3c9965c9f0192a95eb
just-ajs <35227625+just-ajs@users.noreply.github.com>
2023-03-01 10:17:31 +0000

    explanation comments to TexytInput

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -13,12 +13,16 @@
 import { ref } from "vue";
 // Define properties that should come from a parent
 const props = defineProps(["label"]);
+
+// Define emits that you want to send to parent
 const emits = defineEmits(["updateTextInputValue"]);
 
+// Define variables that you want to use
 var textInputValue = ref("default text");
 
-// Define function to emit value form this component to parent
+// Define function that is called v-on:input in your input component (in tempate)
 function updateTextInputValue() {
+  // emit a function called updateTextInputValue (define in emits) with updated value
   emits("updateTextInputValue", textInputValue.value);
 }
 </script>
